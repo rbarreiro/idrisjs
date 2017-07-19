03eb5b4c6546efa720abed14e2b65af8d16ad8b0
Rui Barreiro <rui.barreiro@gmail.com>
2017-07-19 09:22:43 +0100

    fixed sh to compile all examples

--- examples/compileAll.sh
+++ examples/compileAll.sh
@@ -1,5 +1,5 @@
 export IDRISJS_OPTIM=$1
 export IDRISJS_DEBUG=$2
-idris --codegen javascript -p effects -p js ex0.idr -o ex0.js
-idris --codegen javascript -p effects -p js ex1.idr -o ex1.js
-idris --codegen javascript -p effects -p js todo.idr -o todo.js
+idris --codegen javascript -p contrib -p js ex0.idr -o ex0.html
+idris --codegen javascript -p contrib -p js ex1.idr -o ex1.html
+idris --codegen javascript -p contrib -p js todo.idr -o todo.html
