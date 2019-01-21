4b15e86544359d00423dd591738a01d27c6352da
arty <art.yerkes@gmail.com>
2019-01-21 00:52:18 -0500

    Add a simple gamelet that exercises the previous (just a dot running around)

--- examples/compileAll.sh
+++ examples/compileAll.sh
@@ -3,3 +3,4 @@ export IDRISJS_DEBUG=$2
 idris --codegen javascript -p contrib -p js ex0.idr -o ex0.html
 idris --codegen javascript -p contrib -p js ex1.idr -o ex1.html
 idris --codegen javascript -p contrib -p js todo.idr -o todo.html
+idris --codegen javascript -p contrib -p js -p containers game.idr -o game.html
